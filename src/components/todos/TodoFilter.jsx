cf7ddd136f170c64cfa0f8a47b01284fc3200b54
Ndekocode <arickbulakali@gmail.com>
2022-12-11 05:53:26 +0200

    feat: loading action on button

--- src/components/todos/TodoFilter.jsx
+++ src/components/todos/TodoFilter.jsx
@@ -10,14 +10,14 @@ const TodoFilter = ({ filter, onChange }) => {
       <button
         disabled={filter === null}
         onClick={() => onChange(null)}
-        className="px-2 py-2.5 bg-blue-600 rounded-md m-1 text-white disabled:bg-blue-400"
+        className="px-2 py-2.5 bg-blue-600 rounded-md m-1 text-white disabled:bg-blue-400 disabled:opacity-75"
       >
         Aucun filtre
       </button>
       <button
         disabled={filter === true}
         onClick={() => onChange(true)}
-        className="px-2 py-2.5 bg-green-600 text-white rounded-md m-1 disabled:bg-green-400"
+        className="px-2 py-2.5 bg-green-600 text-white rounded-md m-1 disabled:bg-green-400 disabled:opacity-75"
       >
         Completer
       </button>
@@ -25,7 +25,7 @@ const TodoFilter = ({ filter, onChange }) => {
       <button
         disabled={filter === false}
         onClick={() => onChange(false)}
-        className="px-2 py-2.5 bg-red-600 text-white rounded-md m-1 disabled:bg-red-400"
+        className="px-2 py-2.5 bg-red-600 text-white rounded-md m-1 disabled:bg-red-400 disabled:opacity-75"
       >
         A faire{" "}
       </button>
