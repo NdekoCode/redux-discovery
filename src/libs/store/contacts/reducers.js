e886452c278226e803b324e3f9c48cfc62c99cb8
Ndekocode <arickbulakali@gmail.com>
2022-12-30 09:25:59 +0200

    refactor: edit contact func

--- src/libs/store/contacts/reducers.js
+++ src/libs/store/contacts/reducers.js
@@ -12,24 +12,18 @@ export function contactReducer(state = initialState, action) {
         contacts: [action.payload.contact, ...state.contacts],
       };
     case EDIT_CONTACT:
-      const contacts = state.contacts.map((contact) => {
-        if (contact.id.toString() === action.payload.id.toString()) {
-          console.log(action.payload);
-          contact.name = action.payload.name;
-          contact.email = action.payload.email;
-          contact.phone = action.payload.phone;
-          contact.category = action.payload.category;
-          console.log(contact);
-          return contact;
-        }
-        return contact;
-      });
-      return { ...state, contacts };
+      const edit = (contact) =>
+        contact.id === action.payload.id
+          ? { contact, ...action.payload }
+          : contact;
+      return { ...state, contacts: state.contacts.map(edit) };
     case DELETE_CONTACT:
-      const newContacts = state.contacts.filter(
-        (contact) => contact.id !== action.payload.id
-      );
-      return { ...state, contacts: newContacts };
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload.id
+        ),
+      };
     default:
       return state;
   }
