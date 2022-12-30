08b66bcec56ff69d2e4b9b44f6a38bed41a14291
Ndekocode <arickbulakali@gmail.com>
2022-12-30 08:30:44 +0200

    feat: add edit contact action

--- src/libs/store/contacts/actions.js
+++ src/libs/store/contacts/actions.js
@@ -1,10 +1,18 @@
 export const ACTIONS = {
   ADD_CONTACT: "ADD_CONTACT",
+  EDIT_CONTACT: "EDIT_CONTACT",
 };
 export function addContact(contact) {
   const { ADD_CONTACT } = ACTIONS;
   return {
     type: ADD_CONTACT,
     payload: { contact },
+  };
+}
+export function editContact(contact) {
+  const { EDIT_CONTACT } = ACTIONS;
+  return {
+    type: EDIT_CONTACT,
+    payload: contact,
   };
 }
--- src/libs/store/contacts/reducers.js
+++ src/libs/store/contacts/reducers.js
@@ -4,13 +4,24 @@ const initialState = {
   contacts: dataContacts,
 };
 export function contactReducer(state = initialState, action) {
-  const { ADD_CONTACT } = ACTIONS;
+  const { ADD_CONTACT, EDIT_CONTACT } = ACTIONS;
   switch (action.type) {
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [action.payload.contact, ...state.contacts],
       };
+    case EDIT_CONTACT:
+      state.contacts.map((contact) => {
+        if (contact.id === action.payload.id) {
+          contact.name = action.payload.name;
+          contact.email = action.payload.email;
+          contact.phone = action.payload.phone;
+          contact.category = action.payload.category;
+        }
+        return contact;
+      });
+      return state;
     default:
       return state;
   }
